627f071642f3f41fc2edc6c99aca29bae99d3fec
Techhackontime999 <amitkumarkh01012006@gmail.com>
2026-06-21 15:33:41 +0530

    fix: revert entrypoint (don't set DJANGO_ENVIRONMENT), keep ASGI WhiteNoise wrapper + static() URL pattern

--- linkup/entrypoint.sh
+++ linkup/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-export DJANGO_ENVIRONMENT=production
-export DJANGO_SETTINGS_MODULE=professional_network.settings
-
 python manage.py migrate --noinput
 
 exec daphne -b 0.0.0.0 -p 8000 professional_network.asgi:application
