fab4125da2d8b821fdab9a1e8366c87271e309ba
Techhackontime999 <amitkumarkh01012006@gmail.com>
2026-06-21 15:36:26 +0530

    fix: remove WhiteNoise ASGI wrapper (not ASGI-compatible), static() URL pattern is sufficient

--- linkup/professional_network/asgi.py
+++ linkup/professional_network/asgi.py
@@ -3,12 +3,10 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'professional_network.settings')
 
 django_asgi_app = get_asgi_application()
-django_asgi_app = WhiteNoise(django_asgi_app)
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
